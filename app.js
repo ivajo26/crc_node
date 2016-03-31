bacb616a9627b13d5bfeb90ba8782b1d81cf3906
ivajo26 <ivajodimo26@gmail.com>
2016-03-30 21:55:46 -0500

    iterfaz 10%

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ var express = require('express'),
 server.listen(8000);
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + 'index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket) {
-  
+  socket.on('new user', function(data, callback) {
+      if(data in clients){
+        callback(false);
+      }else{
+        socket.username = data;
+        clients[socket.username] = {id:socket.id, connect:false};
+        callback(true);
+        updateUsernames();
+      }
+    });
+
+    socket.on('disconnect', function(data) {
+        if(!socket.username) return;
+        delete clients[socket.username];
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('usernames', clients);
+    }
 });
